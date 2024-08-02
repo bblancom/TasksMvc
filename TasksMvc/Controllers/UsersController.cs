a144c138f1f9447f1f223a5ff6b5c312445f1fbb
Bruno Blanco Martinez <bf.blancomartinez@gmail.com>
2024-08-02 15:16:01 +0200

    Added Register feature using Identity. Controller, View and Model.

---
+++ TasksMvc/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TasksMvc.Models;
+
+namespace TasksMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public UsersController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new IdentityUser()
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, password: model.Password);
+
+            if (!result.Succeeded)
+            {
+                // We add all model errors
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+
+            // If everything went well, we sign in and go to home page
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ TasksMvc/Models/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksMvc.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field needs to be a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
